36e0cab4340cabb80f95ec7958bd757f82cd7660
enes taha sarı <ets.34.es@gmail.com>
2021-10-06 12:05:13 +0300

    add template engine

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
+const ejs = require('ejs')
 const path = require("path");
+
+// TEMPLATE ENGINE
+app.set("view engine","ejs")
 
 // MIDDLEWARES
 app.use(express.static("public"));
-// Selam
 
+// ROUTES
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public/index.html"));
+res.render("index")
+});
+app.get("/about", (req, res) => {
+res.render("about")
+});
+app.get("/add", (req, res) => {
+res.render("add_post")
+});
+app.get("/post", (req, res) => {
+res.render("post")
 });
 
 const port = 3000;
